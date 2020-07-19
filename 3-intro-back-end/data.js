2f431fd9cef8727e629f54daa3cacb4e8436bd93
lucas.brandi <lucas.brandi@wildlifestudios.com>
2020-07-19 01:20:06 -0300

    Refactoring code to use data from server

---
+++ 3-intro-back-end/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: 'VYFL5EjHjGk',
+        title: 'A importância do big data no mercado',
+        duration: '9 min',
+        price: 'Free'
+    },
+    {
+        id: '1B64oqE8PLs',
+        title: 'NoSQL // Dicionário do Programador',
+        duration: '9 min',
+        price: 'Free'
+    },
+    {
+        id: '5b9Z8toVaAU',
+        title: 'O que é Data Science? #HipstersPontoTube',
+        duration: '9 min',
+        price: 'Free'
+    },
+    {
+        id: '4NnuWqQBmLU',
+        title: 'Aprenda Web Scraping em Python',
+        duration: '32 min',
+        price: 'Free'
+    },
+    {
+        id: 'TgiBvKcGL24',
+        title: 'Quick introduction to Apache Spark',
+        duration: '13 min',
+        price: 'Free'
+    },
+    {
+        id: 'S5xF09-wkJw',
+        title: 'Conhecendo a AWS - Amazon Web Service',
+        duration: '49 min',
+        price: 'Free'
+    }
+]
--- 3-intro-back-end/server.js
+++ 3-intro-back-end/server.js
@@ -2,20 +2,34 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data')
 
 server.use(express.static('public'))
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get('/', function(req, res){
-    return res.render('about')
+    const about = {
+        avatar_url: 'https://media-exp1.licdn.com/dms/image/C4E03AQH3kv-QgElQgg/profile-displayphoto-shrink_400_400/0?e=1598486400&v=beta&t=rb0NldRGPNl-BaXbCp7mZRB1dNP8pa5AMe_a180u1eo',
+        name: 'Lucas Brandi',
+        role: 'Aluno - Rocketseat',
+        description: 'Engenheiro de dados buscando compartilhar experiências e incentivar programadores a conhecerem mais sobre o mundo de Big data. Colaborador na <a href="https://wildlifestudios.com" target="_blank">Wildlife Studios</a>.',
+        links: [
+            {name: 'Github', url: 'https://github.com/ldbrandi'},
+            {name: 'Linkedin', url: 'https://www.linkedin.com/in/lucas-brandi/'},
+            {name: 'Twitter', url: 'https://twitter.com/ldbrandi_19'}
+        ]
+    }
+
+    return res.render('about', {about})
 })
 
 server.get('/portfolio', function(req, res){
-    return res.render('portfolio')
+    return res.render('portfolio', {items: videos})
 })
 
 server.listen(5000, function(){
