33d4bcd07ec275802d94508f4c5149e642548903
lucas.brandi <lucas.brandi@wildlifestudios.com>
2020-08-02 18:47:21 -0300

    Add challenge 3-2

---
+++ 3-challenge/data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        img_url: 'https://app.rocketseat.com.br/static/media/Starter.64c237cc.svg',
+        title: 'Starter',
+        modules: '5 módulos',
+        price: 'Free'
+    },
+    {
+        img_url: 'https://app.rocketseat.com.br/static/media/LaunchBase.08285320.svg',
+        title: 'Launchbase',
+        modules: '9 módulos',
+        price: 'R$ 960'
+    },
+    {
+        img_url: 'https://app.rocketseat.com.br/static/media/GoStack.83a178a0.svg',
+        title: 'GoStack',
+        modules: '12 módulos',
+        price: 'R$ 1400'
+    }
+]
--- 3-challenge/server.js
+++ 3-challenge/server.js
@@ -2,20 +2,40 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const courses = require('./data')
 
 server.use(express.static('public'))
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get('/', function(req, res){
-    return res.render('about')
+    const about = {
+        avatar_url: 'https://pbs.twimg.com/profile_images/1271517147349626881/Mf1UjRa0_400x400.jpg',
+        name: 'Rocketseat',
+        title: 'Ready for the next level?',
+        description: 'A Rocketseat é uma escola de programação focada nas tecnologias em alta do mercado. Confira mais <a href="https://rocketseat.com.br" target="_blank">aqui!</a>',
+        stack: 'Nossa Stack:',
+        stack_icons: [
+            {name: 'HTML', url: 'assets/html5.png'},
+            {name: 'CSS', url: 'assets/css3.png'},
+            {name: 'JavaScript', url: 'assets/js.png'}
+        ],
+        links: [
+            {name: 'Github', url: 'https://github.com/ldbrandi'},
+            {name: 'LinkedIn', url: 'https://www.linkedin.com/in/lucas-brandi/'},
+            {name: 'Twitter', url: 'https://twitter.com/ldbrandi_19'}
+        ]
+    }
+
+    return res.render('about', {about})
 })
 
 server.get('/courses', function(req, res){
-    return res.render('courses')
+    return res.render('courses', {items: courses})
 })
 
 server.use(function(req, res) {
